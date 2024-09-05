5fff19a03262f1feabc93de8787a653f5a47ff82
Nyadej <naomiadejumo@gmail.com>
2024-09-05 14:41:35 +0100

    trying to add logic for user selection between teacher and student registration and trying to create - storing IDs and names in hashmaps.

--- Main-project/StudentDatabase.java
+++ Main-project/StudentDatabase.java
@@ -1,8 +1,38 @@
 import java.util.Scanner;
 
 public class StudentDatabase {
-        public static void main(String[] args) {
-    
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        HashMap<>
+
+        System.ount.print("Are you a Teacher or Student? (Enter 'T' for Teacher, 'S' for student): ");
+
+        Char role = scanner.nextLine().toUpperCase();
+
+        switch (role) {
+            case "T":
+                System.out.print("How many teachers will be registering today?: ");
+                int nTeachers = scanner.nextInt();
+                scanner.nextLine();
+
+                HashMap<Integer, String>
+
+                break;
+
+            case "S":
+                System.out.print("How many students will be registering today?: ");
+                Student student = new Student(scanner);
+
+
+            default:
+                System.out.println("Please select a vaild option. Valid options are T or S. Restart the programme.");
+
+        }
+    }
+}
+
             // Prompt the user for the number of students
             System.out.print("How many students will be registering today?: ");
             Scanner scanner = new Scanner(System.in);
